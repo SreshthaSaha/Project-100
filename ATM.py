60f3590a436d8d1a28861ad90ff8a0681b1dac37
SreshthaSaha <65612941+SreshthaSaha@users.noreply.github.com>
2021-05-23 08:37:22 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,15 +1,20 @@
 class ATM:
-    def __init__(self,card_number,pin_nmber):
+    def __init__(self):
+        card_number = input("Please enter your card number: ")
+        pin_nmber = input("Enter your pin number: ")
         self.card_number = card_number
         self.pin_nmber = pin_nmber
 
+
     def CashWithdrawl(self):
         print("Cash has been withdrawed")
+        print("Your card number is : " + self.card_number)
     def BalanceEnquiry(self):
-        print("Your balance amount left is: ''")
+        print("Your balance amount left is: 'xxx'")
+        print("Your pin number is: " + self.pin_nmber)
 
-test1 = ATM(123,456)
+test1 = ATM()
 test1.CashWithdrawl()
 
-test2 = ATM(789,109)
+test2 = ATM()
 test2.BalanceEnquiry()
